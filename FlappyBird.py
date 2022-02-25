84aaf0df5a8f714d9010e42af0b0d46e0928b0e5
Mateus Zanetti Camargo Penteado <matezcp@hotmail.com>
2022-02-25 00:46:34 -0300

    Minor changes

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,20 +1,14 @@
 import pygame
-import os
-import random
+from passaro import Passaro
+from chao import Chao
+from cano import Cano
 
 pygame.font.init()
 
 TELA_ALTURA = 800
 TELA_LARGURA = 500
 
-IMG_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
-IMG_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','base.png')))
 IMG_FUNDO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bg.png')))
-IMGS_PASSARO = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird3.png'))),
-]
 
 FONT_PONTOS = pygame.font.SysFont('arial', 50)
 
